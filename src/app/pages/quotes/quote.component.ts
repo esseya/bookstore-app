0e7452042b4d66880f93c98a58d7145b6607e52c
Eza <eseasm@gmail.com>
2025-06-16 14:39:37 +0200

    updated with import

--- src/app/pages/quotes/quote.component.ts
+++ src/app/pages/quotes/quote.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-quote',
-  standalone: true,  
+  standalone: true,
   templateUrl: './quote.component.html',
-  imports: [],           
+  imports: [CommonModule], 
 })
 export class QuoteComponent {
   quotes: string[] = [
